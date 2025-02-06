bb2a02e32277ecabcc18bd30587baf800bdbdfa6
ymh386 <dbsansgus0602@naver.com>
2025-02-06 14:43:25 +0900

    COUNTRIES테이블 DTO변수, DAO메서드선언

---
+++ src/main/java/com/winter/app/countries/CountryDAO.java
@@ -0,0 +1,42 @@
+package com.winter.app.countries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryDAO {
+	
+	public List<CountryDTO> getList() throws Exception {
+		List<CountryDTO> ar = new ArrayList<CountryDTO>();
+		
+		
+		return ar;
+	}
+	
+	public CountryDTO getDetail(CountryDTO countryDTO) throws Exception {
+		
+		
+		
+		return countryDTO;
+	}
+	
+	public int add(CountryDTO countryDTO) {
+		int result = 0;
+		
+		
+		return result;
+	}
+	
+	public int delete(CountryDTO countryDTO) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	public int update(CountryDTO countryDTO) {
+		int result = 0;
+		
+		
+		return result;
+	}
+
+}
